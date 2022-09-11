26c6c51f24f3523ceb7608fd8088fd1f0a03ecc1
GabiYuri <gabriela.ishikawa@gmail.com>
2022-09-11 20:09:56 -0300

    color correction new edital

--- scripts/DiseaseDetection.py
+++ scripts/DiseaseDetection.py
@@ -6,36 +6,29 @@ import numpy as np
 class diseaseDetection:
 
     def __init__(self):
-        video_path = "/home/renato/skyrats_ws2/src/IMAV2022Indoor/images/video1.mp4"
-        #self.cap = cv2.VideoCapture("/dev/video0")
-        self.cap = cv2.VideoCapture(video_path)
+        self.image = cv2.imread("/home/gabs/skyrats_ws/src/IMAV2022Indoor/scripts/planta.png")
+        self.min_green = (35, 64, 70)
+        self.max_green = (80, 255, 231)
+        self.min_brown = (11, 4, 133)
+        self.max_brown = (76, 52, 255)
 
     def cropImage(self, image):
         #Split an image 1920x1080 in 6 pieces  960x540
-
         croppedImage = []
-        
         for i in range(0,6):
             if i < 3:
-                
                 piece = image[0:270, i*320:(i+1)*320]
                 croppedImage.append(piece)
                 
-                
-            if i >= 3:
-                
+            if i >= 3:  
                 piece = image[270:540, (i-3)*320:(i-2)*320]
                 croppedImage.append(piece)
-
-            
 
         return croppedImage
 
     def recomposeImage(self, images):
         #receives cropped image array and returns the original image
-
         result = np.zeros(self.image.shape, np.uint8)
-
         for i in range(0, 6):
             if i < 3:
                 result[0:270, i*320:(i+1)*320] = images[i]
@@ -65,7 +58,7 @@ class diseaseDetection:
         mask = cv2.bitwise_not(mask)
         result = cv2.bitwise_and(mask, imageCopy)
         hsv = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
-        brownMask = cv2.inRange(hsv, (0, 0, 0), (14, 255, 255))
+        brownMask = cv2.inRange(hsv, self.min_brown, self.max_brown)
         brownMask = brownMask>0
         imageBrown = np.zeros(image.shape, np.uint8)
         imageBrown[brownMask] = result[brownMask]
@@ -79,49 +72,32 @@ class diseaseDetection:
         return result
 
     def detectionLoop(self):
-        while self.cap.isOpened():
+        imageHSV = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        #saturation = imageHSV[..., 1]
+        #saturation = cv2.add(saturation, 150)
 
-            success,self.image = self.cap.read()
-            
-            self.image = cv2.resize(self.image, (960,540))
+        greenMask = cv2.inRange(imageHSV, self.min_green, self.max_green)
+        imask = greenMask>0
+        imageGreen = np.zeros_like(self.image, np.uint8)
+        imageGreen[imask] = self.image[imask]
+        imageGreen_copy = np.copy(self.image)
 
+        '''
+        croppedImage = self.cropImage(imageGreen)
+        imageCopy = self.cropImage(self.image)
 
-            imageHSV = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-            #saturation = imageHSV[..., 1]
-            #saturation = cv2.add(saturation, 150)
+        processedImage = []
+        for i in range(0, 6):
+            processedImage.append(self.imageProcessing(croppedImage[i], imageCopy[i]))
 
-            greenMask = cv2.inRange(imageHSV, (29, 29, 0), (101, 255, 210))           
-            #greenMask = cv2.inRange(imageHSV, (31, 34, 0), (179, 255, 255))
-            #greenMask = cv2.inRange(imageHSV, (59, 8, 138), (98, 24, 255))
-            #greenMask = cv2.inRange(imageHSV, (75, 145, 130), (149, 250, 66))
-            #greenMask = cv2.inRange(imageHSV, (36, 25, 25), (86, 255, 255))
-            #greenMask = cv2.inRange(imageHSV, (40, 40, 40), (70, 255, 255))
-            #greenMask = cv2.inRange(imageHSV, (26, 10, 20), (97, 255, 255))
+        result = self.recomposeImage(processedImage)
+        '''
 
-            #greenMask = cv2.inRange(imageHSV, (0,0,0), (87,255,255))
-            imask = greenMask>0
-
-            imageGreen = np.zeros_like(self.image, np.uint8)
-            imageGreen[imask] = self.image[imask]
-            croppedImage = self.cropImage(imageGreen)
-            imageCopy = self.cropImage(self.image)
-            processedImage = []
-
-
-            for i in range(0, 6):
-
-                processedImage.append(self.imageProcessing(croppedImage[i], imageCopy[i]))            
-
-            result = self.recomposeImage(processedImage)
-        
-            cv2.namedWindow("result", cv2.WINDOW_FULLSCREEN)
-            cv2.imshow("result", result)
-            cv2.namedWindow("imageGreen", cv2.WINDOW_FULLSCREEN)
-            cv2.imshow("imageGreen", imageGreen)
-
-            if cv2.waitKey(5) & 0xFF == 27:
-                
-                break
+        result = self.imageProcessing(imageGreen, imageGreen_copy)
+    
+        cv2.namedWindow("result", cv2.WINDOW_FULLSCREEN)
+        cv2.imshow("result", result)
+        cv2.waitKey(0)
     
     def main_interface(self):
         self.detectionLoop()
